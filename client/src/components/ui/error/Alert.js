3650c3f06e3e309d2c8dd718a5958268eb4d6497
Mike Daley <michaelcdaley@yahoo.com>
2020-06-09 07:21:18 -0700

    Fixed bug w/ ErrorAlert that caused app to crash when the user was not authorized

--- client/src/components/ui/error/Alert.js
+++ client/src/components/ui/error/Alert.js
@@ -9,14 +9,16 @@ import { useHistory }   from 'react-router-dom'
  * If the user is not authorized then redirect the user to the /login page. If
  * there is an error than return a component that displays the error.
  * 
- * @props {Object} message - error message
+ * @props {Object} error - Error object w/ code and message.
  */
 const ErrorAlert = ({error}) => {
   let history = useHistory()
 
-  return error.code === 401 ? ( 
+  if(error.code === 401) {
     history.push('/login', {message: 'Please log in'})
-  ) : (
+  }
+
+  return (
     <Alert variant='danger'>
       {error.message}
     </Alert>
